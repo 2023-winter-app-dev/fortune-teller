d8ade768c17345f83cde9974edfd4a33895ad2b6
cwen-booth <cwen@chicagobooth.edu>
2023-01-26 01:07:01 +0000

    Class-time Part II Completed

--- app/controllers/numbers_controller.rb
+++ app/controllers/numbers_controller.rb
@@ -4,10 +4,22 @@ class NumbersController < ApplicationController
 
     5.times do
       giraffe = rand(1...100)
-      
+
       @zebra.push(giraffe)
     end
 
-    render({ :template => "lottery_stuff/woohoo.html.erb"})
-  end 
+    render({ :template => "lottery_stuff/woohoo.html.erb" })
+  end
+
+  def losers
+    @alligator = Array.new
+
+    5.times do
+      rhinocerous = rand(1..100)
+
+      @alligator.push(rhinocerous)
+    end
+
+    render({ :template => "lottery_stuff/boohoo.html.erb" })
+  end
 end
